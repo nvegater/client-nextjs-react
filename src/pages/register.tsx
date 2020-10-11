47c2b04bafccf05bd4f696543d1c9dd2337428ce
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-10-11 18:29:02 +0200

    added formik

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -8,35 +8,39 @@ interface RegisterProps {
 
 const Register: FC<RegisterProps> = () => {
     
-    let handleSubmit = (values, actions) => {
+    let handleSubmit = (props) => {
         setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            actions.setSubmitting(false);
+            alert(JSON.stringify(props.values, null, 2));
+            // actions has callbacks to influence the form
+            props.actions.setSubmitting(false);
         }, 1000);
     };
-    return <Formik onSubmit={handleSubmit} initialValues={{name: "Insert something"}}>
-        {
-            props => (
-                <form onSubmit={props.handleSubmit}>
-                    <Field name="name">
-                        {({field, form}) => (
-                            <FormControl isInvalid={form.errors.name && form.touched.name}>
-                                <FormLabel htmlFor="name">First name</FormLabel>
-                                <Input {...field} id="name" placeholder="name"/>
-                                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                            </FormControl>
-                        )}
-                    </Field>
-                    <Button
-                        mt={4}
-                        variantColor="teal"
-                        isLoading={props.isSubmitting}
-                        type="submit"
-                    >
-                        Submit
-                    </Button>
-                </form>
-            )}
+    return <Formik initialValues={{name: "Insert something"}} onSubmit={handleSubmit}>
+            {
+                    ({
+                     handleSubmit,
+                     isSubmitting
+                    }) => (
+                    <form onSubmit={handleSubmit}>
+                        <Field name="name">
+                            {({field, form}) => (
+                                <FormControl isInvalid={form.errors.name && form.touched.name}>
+                                    <FormLabel htmlFor="name">First name</FormLabel>
+                                    <Input {...field} id="name" placeholder="name"/>
+                                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                                </FormControl>
+                            )}
+                        </Field>
+                        <Button
+                            mt={4}
+                            variantColor="teal"
+                            isLoading={isSubmitting}
+                            type="submit"
+                        >
+                            Submit
+                        </Button>
+                    </form>
+                )}
     </Formik>
 }
 
