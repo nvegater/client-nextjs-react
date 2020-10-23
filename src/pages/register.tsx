9f7fd6eef96ebb3ae2561bea611ffe353cfefed2
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-10-23 15:06:48 +0200

    handle errors from graphql-server and pass them to Formik errors for input validation

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,39 +3,26 @@ import {Form, Formik} from "formik";
 import InputField from "../components/InputField";
 import {Button} from "@chakra-ui/core";
 import {useRegisterMutation} from "../generated/graphql";
-
-//For advanced usage: https://formik.org/docs/api/useFormik
-// import {useFormik} from "formik";
-/*const formik = useFormik({
-    initialValues: {username: "", password: ""},
-    onSubmit: values => {
-        alert(JSON.stringify(values, null, 2));
-    },
-})*/
+import {toErrorMap} from "../utils/toErrorMap";
 
 const Register: FC = () => {
 
-    const [, updateRegister] = useRegisterMutation()
+    const [, register] = useRegisterMutation()
 
     return <Formik initialValues={{username: "", password: ""}}
-                   onSubmit={
-                       async (props) => {
-                           console.log(props)
-                           const response = await updateRegister(props);
-                           console.log(response.data?.register.user)
-                           console.log(response.data?.register.errors)
+                   onSubmit={async (values, {setErrors}) => {
+                           const response = await register(values);
                            if (response.data?.register.errors){
-                               // handle error with formik
+                               setErrors(toErrorMap(response.data.register.errors))
                            }
-                           return response
                        }
                    }
     >
         {
             ({isSubmitting}) => (
                 <Form>
-                    <InputField label="Username" name="username"/>
-                    <InputField label="Password" name="password" type="password"/>
+                    <InputField label="Username" name="username" placeholder="username"/>
+                    <InputField label="Password" name="password" placeholder="password" type="password"/>
                     <Button type="submit" color="teal" isLoading={isSubmitting} m={5}>Register</Button>
                 </Form>
             )
