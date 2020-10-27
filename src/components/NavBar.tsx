d9f6d2fa98e1326c027a49b2f04a2d75bf05d34b
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-10-27 17:42:08 +0100

    add "me" query, generated Types and add navbar request process handling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,36 @@
 import React from "react";
-import {Box, Link} from "@chakra-ui/core";
+import NextLink from "next/link"
+import {Button, Link} from "@chakra-ui/core";
+import FormResponsiveContainer from "./FormResponsiveContainer";
+import {useMeQuery} from "../generated/graphql";
 
-const NavBar:React.FC = ()=>{
-    return <Box>
-        <Link>Login</Link>
-        <Link>Register</Link>
-    </Box>
+const NavBar: React.FC = () => {
+    const [{data, fetching}] = useMeQuery()
+    const me = data?.me;
+
+    return (
+        <FormResponsiveContainer>
+        {
+            fetching && <h1>Loading</h1>
+        }
+        {
+            !fetching && !me && <>
+                <NextLink href="/login">
+                    <Link m={7}>Login</Link>
+                </NextLink>
+                <NextLink href="/register">
+                    <Link m={7}>Register</Link>
+                </NextLink>
+            </>
+        }
+        {
+            me && <>
+                <h1>Welcome {me.username}</h1>
+                <Button type="submit" m={5}>Logout</Button>
+            </>
+        }
+
+    </FormResponsiveContainer>)
 }
 
 export default NavBar;
