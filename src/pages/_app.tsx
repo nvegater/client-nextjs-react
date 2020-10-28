57d6bf1b369d5c9f079a7146939095c96f0bc5e4
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-10-28 14:38:30 +0100

    add register cache resolver to update cache when the registration is successful.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import {ThemeProvider, CSSReset, ColorModeProvider} from '@chakra-ui/core';
 import {createClient, dedupExchange, fetchExchange, Provider} from 'urql';
 import {AppProps} from "next/app";
-import {cacheExchange, Cache, QueryInput, Data, UpdateResolver} from '@urql/exchange-graphcache';
-import {LoginMutation, MeDocument, MeQuery} from "../generated/graphql";
+import {cacheExchange, Cache, QueryInput, UpdateResolver} from '@urql/exchange-graphcache';
+import {LoginMutation, MeDocument, MeQuery, RegisterMutation} from "../generated/graphql";
 import theme from '../theme';
 import {CacheExchangeOpts} from "@urql/exchange-graphcache/dist/types/cacheExchange";
 
@@ -22,7 +22,15 @@ const updateLoginCache:(result: LoginMutation, query: MeQuery) => MeQuery = (res
         }
 };
 
-const loginCacheResolver: UpdateResolver = (result, args, cache, info) => {
+const updateRegisterCache:(result: RegisterMutation, query: MeQuery) => MeQuery = (result, query) => {
+    return result.register.errors
+        ? query
+        : { //insert result of login on cache.
+            me: result.register.user
+        }
+};
+
+const loginCacheResolver: UpdateResolver = (result, args, cache, _info) => {
     //Wrapper to get Types
     typedResolver<LoginMutation, MeQuery>(
         cache,
@@ -32,10 +40,21 @@ const loginCacheResolver: UpdateResolver = (result, args, cache, info) => {
     )
 };
 
+const registerCacheResolver: UpdateResolver = (result, args, cache, _info) => {
+    //Wrapper to get Types
+    typedResolver<RegisterMutation, MeQuery>(
+        cache,
+        {query: MeDocument},
+        result,
+        updateRegisterCache
+    )
+};
+
 const cacheLoginMutation: CacheExchangeOpts = {
     updates: {
         Mutation: {
-            login: loginCacheResolver
+            login: loginCacheResolver,
+            register: registerCacheResolver
         }
     }
 };
