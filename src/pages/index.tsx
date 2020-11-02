5a1f3bd4d26b07ce772b016fb96b583235b078cd
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-11-02 14:55:05 +0100

    Generate "Posts" types from query and request posts with server side rendering.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,34 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import {withUrqlClient} from "next-urql";
 import {createUrqlClient} from "../graphql/urqlProvider";
+import {usePostsQuery} from "../generated/graphql";
 
-const Index = () => <>
-    <NavBar/>
-</>
+const Index = () => {
+    const [{data:postsResponse, fetching}] = usePostsQuery()
+    return <>
+        <NavBar/>
+        {
+            fetching && <h1>Loading</h1>
+        }
+        {
+            !fetching && postsResponse && <>
+                {
+                    postsResponse.posts.length > 0 && <>
+                        {
+                            postsResponse.posts.map((post)=>
+                                <div key={post.id}>{post.title}</div>
+                            )
+                        }
+                    </>
+                }
+                {
+                    postsResponse.posts.length === 0 && <>
+                        <h1>No posts available</h1>
+                    </>
+                }
+            </>
+        }
+    </>
+}
 
-
-export default withUrqlClient(createUrqlClient, {ssr:false})(Index)
+export default withUrqlClient(createUrqlClient, {ssr:true})(Index)
