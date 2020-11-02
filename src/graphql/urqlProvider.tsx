109ce26f92119c060caf03ae311c260577f48e81
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-11-02 08:02:09 +0100

    type safe urql client

--- src/graphql/urqlProvider.tsx
+++ src/graphql/urqlProvider.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import {dedupExchange, Exchange, fetchExchange} from 'urql';
 import {cacheExchange} from '@urql/exchange-graphcache';
 import cacheUpdates from "../graphql/cache/resolvers";
-import {SSRExchange} from "next-urql/dist/types/types";
+import {NextUrqlClientConfig, SSRExchange} from "next-urql/dist/types/types";
 
-export const createUrqlClient = (ssrExchange: SSRExchange) => {
+export const createUrqlClient:NextUrqlClientConfig = (ssrExchange: SSRExchange) => {
     const castedExchanges: Exchange[] = [ //Needed because [Exchange, Exchange] not the Same as Exchange[].. duh
         dedupExchange,
         cacheExchange(cacheUpdates),
