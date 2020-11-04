0e1f38dcf56a87fd13fdb3337381a8c94e99b021
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-11-04 14:08:24 +0100

    add form to change password

--- src/pages/change-password/[token].tsx
+++ src/pages/change-password/[token].tsx
@@ -1,19 +1,39 @@
 import React from "react";
 import {NextPage} from "next";
+import {Form, Formik, FormikHelpers} from "formik";
+import InputField from "../../components/InputField";
+import {Button} from "@chakra-ui/core";
+import FormResponsiveContainer from "../../components/FormResponsiveContainer";
 
 interface ChangePasswordProps {
-    token:string;
+    token: string;
 }
-export const ChangePassword:NextPage<ChangePasswordProps> = ({token}) => {
 
-    return (<>
-    <div>Token is {token}</div>
-    </>);
+interface NewPassword {
+    newPassword: string;
+}
+
+export const ChangePassword: NextPage<ChangePasswordProps> = ({token}) => {
+    const initialNewPasswordValue: NewPassword = {newPassword: ""};
+    const handleLoginSubmit = async (values: NewPassword, errors: FormikHelpers<NewPassword>) => {
+        console.log(values, errors)
+    };
+
+    return (<FormResponsiveContainer>
+        <Formik initialValues={initialNewPasswordValue} onSubmit={handleLoginSubmit}>
+            {
+                ({isSubmitting}) => (
+                    <Form>
+                        <InputField label="New password" name="newPassword" placeholder="New password" type="password"/>
+                        <Button type="submit" isLoading={isSubmitting} m={5}>Change password</Button>
+                    </Form>
+                )
+            }
+        </Formik>
+    </FormResponsiveContainer>);
 }
 
 ChangePassword.getInitialProps = ({query}) => {
-    console.log("This is the URL /change-password/[whateverTokenIS]", query)
-    console.log("Token received in the email: [whateverTokenIS]", query.token)
     return {
         token: query.token as string
     }
